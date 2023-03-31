ea5297225b177ca853eb720a8064d15f696218ba
Ackon Samuel <ackons433@gmail.com>
2023-03-31 08:54:56 +0000

    Set up routes for user authentication endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  devise_for :users, path: 'auth/users', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup',
+    password: 'password'
+  },
+controllers: {
+sessions: 'users/sessions',
+registrations: 'users/registrations',
+confirmations: 'users/confirmations',
+passwords: 'users/passwords'
+}
 end
