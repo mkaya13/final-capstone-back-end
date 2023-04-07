94f597f2ecc9094f4a761760a50d99f19d2f9445
mkaya13 <53873573+mkaya13@users.noreply.github.com>
2023-04-07 15:40:54 +0300

    Fixes linters

--- app/controllers/doctors_controller.rb
+++ app/controllers/doctors_controller.rb
@@ -14,17 +14,7 @@ class DoctorsController < ApplicationController
 
   def create
     @doctor = Doctor.new(doctor_params)
-    # @doctor.user = current_user
-
     @doctor.save
-
-    # respond_to do |format|
-    #   if @doctor.save
-    #     format.html { redirect_to doctors_url, notice: 'Doctor was successfully created.' }
-    #   else
-    #     flash[:error] = 'Error: doctor could not be saved'
-    #   end
-    # end
   end
 
   def doctor_params
--- app/models/doctor_appointment_time.rb
+++ app/models/doctor_appointment_time.rb
@@ -2,11 +2,9 @@ class DoctorAppointmentTime < ApplicationRecord
   has_many :doctor_times
   belongs_to :doctor
 
-
   validates :doctor_time_id, presence: true
   validates :doctor_id, presence: true
   validates :date, presence: true
   validates :doctor_time_id, presence: true
   validates :available, presence: true
-
 end
