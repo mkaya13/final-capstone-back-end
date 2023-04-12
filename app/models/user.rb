75d4597e475e1a2f29b63d16201be4c50cb3a7e7
Ackon Samuel <ackons433@gmail.com>
2023-04-12 12:46:21 +0000

    Hot fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-
   include Devise::JWT::RevocationStrategies::JTIMatcher
   devise :database_authenticatable, :registerable, :validatable,
          :jwt_authenticatable, :recoverable, :rememberable, jwt_revocation_strategy: self
