54b56591a4a8d665005b41c82480f4f5c66f3fe8
Trust7 <simeontrust7@gmail.com>
2023-04-12 17:09:02 +0100

    Made association between doctor and doctor_appointment_time model

--- app/models/doctor.rb
+++ app/models/doctor.rb
@@ -2,6 +2,7 @@ class Doctor < ApplicationRecord
   has_many :doctor_times
   has_many :appointments
   has_many :time_schedules
+  has_many :doctor_appointment_times
 
   validates :first_name, presence: true
   validates :last_name, presence: true
