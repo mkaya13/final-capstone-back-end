0e35f15974f3fc8573e6d80610e8f3802bf5bd50
mkaya13 <53873573+mkaya13@users.noreply.github.com>
2023-04-12 23:05:16 +0300

    Adds create and destroy doctor endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -18,7 +18,7 @@ passwords: 'users/passwords'
 
   namespace :api do
     namespace :v1, defaults: { format: :json } do
-      resources :doctors, only: [:index, :show] do
+      resources :doctors do
         resources :doctor_appointment_times, only: [:index]
       end
 
